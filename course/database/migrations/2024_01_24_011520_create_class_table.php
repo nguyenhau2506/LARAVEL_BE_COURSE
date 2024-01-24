4c658c1c41fa478c79c77c3adcc261c0fc04d47a
thong89x <thong89x@gmail.com>
2024-01-24 08:23:33 +0700

    add start and execute script, migrate class table

---
+++ course/database/migrations/2024_01_24_011520_create_class_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->id();
+            $table->string("name",254)->nullable();
+            $table->string("code",254)->nullable();
+            $table->string("description",254)->nullable();
+            $table->bigInteger("owner_id")->unsigned()->nullable();
+            $table->boolean('is_joinable')->default(true);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
